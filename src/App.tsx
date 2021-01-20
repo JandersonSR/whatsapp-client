a83a41b02812e89213b6a859f9fcf5967f3bc9c4
robertvitoriano <robertvitorianodev@gmail.com>
2021-01-20 16:01:40 -0300

    receive image from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState, MouseEvent } from 'react';
 import api from './services/api'
 import socketIOClient, { Socket } from "socket.io-client";
-import QRCode from "react-qr-code";
+import  QRCode  from 'qrcode.react';
 
 function App() {
   const [socket, setSocket] = useState<Socket>()
-  const [qrCode, setQrCode] = useState(null)
+  const [qrCode, setQrCode] = useState("1@dsBA9duO0/u2qOmT1FdDmOBYH0oFE6dWiIL7730rC9WeoB277aUmL1n5D4KW6lpd/Z1C7IIoWzZf8w==,TTpYk9kx/2asE9llELuc0sbwPxiis9e0dCVmk+gCjT4=,6LPEwc7XD1kaMEfBsRBfhQ==")
+  const [qrCodeImage, setQrCodeImage] = useState<string>()
 
   const ENDPOINT = "http://localhost:5000";
 
@@ -36,8 +37,8 @@ function App() {
 
       <h1>Hello World</h1>
             {/*@ts-ignore*/}
-{ qrCode&&<QRCode value={qrCode} />}
-
+{/* { qrCode&&<QRCode value={"1@dsBA9duO0/u2qOmT1FdDmOBYH0oFE6dWiIL7730rC9WeoB277aUmL1n5D4KW6lpd/Z1C7IIoWzZf8w==,TTpYk9kx/2asE9llELuc0sbwPxiis9e0dCVmk+gCjT4=,6LPEwc7XD1kaMEfBsRBfhQ=="} style={{height:280,width:180}} />} */}
+      <img src={`data:image/png;base64,${qrCodeImage}`}/>
     </div >
   );
 }
