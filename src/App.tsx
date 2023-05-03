58d222c9319f108b8aac42d25197ab9f4bfc50db
JandersonSR <jandersonrodriguesir@gmail.com>
2023-05-03 17:42:51 -0300

    change css for whatsclient

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, MouseEvent } from 'react';
 import api from './services/api'
 import socketIOClient, { Socket } from "socket.io-client";
+
 // import  QRCodeReact  from 'qrcode.react';
 //@ts-ignore
 // import QRCodeReact  from 'qrcode-react';
@@ -11,7 +12,7 @@ import wppLogo from './whatsapp-black-logo.png'
 interface IContact{
   name?:string
   number?:string
-  
+
 }
 
 function App() {
@@ -26,14 +27,15 @@ function App() {
   const [message, setMessage] = useState<string>()
   const [csv, setCSV] = useState<FileList>()
 
+  const customToken = '?bearer=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWZhZmZhY2VmMWI5NjJhMGFlZjhmMCIsImxldmVsIjoic3RvcmUiLCJ1c2VyTmFtZSI6IkNodW5MaSBMb2phIiwidXNlckVtYWlsIjoiamFtaWVsdGVyY2Vpcm9hZG1AeW9wbWFpbC5jb20iLCJzeXN0ZW1JZCI6IjYxMWZhOWM4Y2VmMWI5NjJhMGFlZjdmNiIsIm1hc3RlcklkIjoiNjExZmE5YzhjZWYxYjk2MmEwYWVmN2Y2IiwibWFzdGVyTmFtZSI6IktlbiBNYXN0ZXJzIiwiY29ycG9yYXRpb25JZCI6IjYxMWZhZDk3Y2VmMWI5NjJhMGFlZjg4OSIsImNvcnBvcmF0aW9uTmFtZSI6IlJ5dSBDb3JwcyIsInN0b3JlSWQiOiI2MTFmYWZmYWNlZjFiOTYyYTBhZWY4ZjAiLCJtYWluIjp0cnVlLCJpbWFnZSI6Imh0dHBzOi8vY29yZXZpc2lvbmJ1Y2tldC5zMy5zYS1lYXN0LTEuYW1hem9uYXdzLmNvbS9LZW4lMjBNYXN0ZXJzL3VuYXV0aC8wMTA5NWQzYTFjYTIzZWFiMzJhMTFjMjc2YWRhMWIzMDQ1ZWNhZGExX2hxLmpwZzIwLTA4LTIwMjEtMTAlM0EzNiUzQTU4LW0lM0E2MTFmYTljOGNlZjFiOTYyYTBhZWY3ZjYtYyUzQTYxMWZhZDk3Y2VmMWI5NjJhMGFlZjg4OS11JTNBNjExZmFkOTdjZWYxYjk2MmEwYWVmODg5LV9hdXRoX2FnZW5jaWVzLmpwZWciLCJhZmZpbGlhdGUiOnRydWUsInJlZnJlc2hUb2tlbklkIjoiNjNhMzAyNDUwZjM5ZmUyNmRjOTAyN2I4IiwicGVybWlzc2lvbnMiOnt9LCJpYXQiOjE2NzE2MjczMzMsImV4cCI6MTY3MzEyNzMzM30.9MR5UEizh7da8SFegz739kqfkjbM5lBkVVPc0yvGQ_A'
 
+  // const ENDPOINT = "https://sms-production-server.herokuapp.com";
 
-  const ENDPOINT = "https://sms-development-server.herokuapp.com";
-
-  // const ENDPOINT = "http://localhost:4000"
+  const ENDPOINT = "http://10.10.0.109:4000"
+  // const ENDPOINT = "http://10.10.0.109:5003"
   useEffect(() => {
     // @ts-ignore
-    setSocket(socketIOClient(ENDPOINT));
+    setSocket(socketIOClient(`${ENDPOINT}/${customToken}`));
 
   }, [ENDPOINT]);
 
@@ -41,14 +43,14 @@ function App() {
   useEffect(() => {
     if (socket) {
 
-      socket.on("qr", (qrCode: string) => {
+      socket.on("whatsapp-qrCode", (qrCode: string) => {
          setQrCode(qrCode)
          console.log("qrCode",qrCode)
       })
-      socket.on("file", (file:{qr:string}) => {
+      socket.on("whatsapp-file", (file:{qr:string}) => {
         console.log("file",file )
         setQrCodeFile(file.qr)
-       
+
      })
 
      socket.on("authentication", (auth:boolean) => {
@@ -57,18 +59,30 @@ function App() {
    })
    socket.on("disconnection", (auth:boolean) => {
     console.log("Has disconnected")
-   setHasConnected(false)
+    setHasConnected(false)
 })
 
     }
   }, [socket])
 
+  // const handleGenerateQrCode = async () => {
+  //   const response = await api.post("/unauth/whatsapp/qrcode");
+
+  //   console.log("Minha response",response)
+  //   console.log("qrCode", response.data?.qrCode?.clientQRCode)
+
+  //   setQrCode(response.data?.qrCode?.clientQRCode)
+
+  //   console.log("file",response?.data?.qrCode?.file )
+  //   setQrCodeFile(response?.data?.qrCode?.file?.qr)
+  // }
+
  const handleConnection = async ()=>{
-   const response = await api.post("/whatsapp/create/session");
+   const response = await api.post("/unauth/whatsapp/create/session");
 
    console.log("Minha response",response)
  }
- 
+
   // handle input change
       //@ts-ignore
   const handleInputChange = (e, index) => {
@@ -79,14 +93,14 @@ function App() {
     list[index][name] = value;
     setInputList(list);
   };
- 
+
   // handle click event of the Remove button
   const handleRemoveClick = (index:number )=> {
     const list = [...inputList];
     list.splice(index, 1);
     setInputList(list);
   };
- 
+
   // handle click event of the Add button
   const handleAddClick = () => {
     setInputList([...inputList, { name: "", number: "" }]);
@@ -120,7 +134,7 @@ function App() {
   const  handleSendCSV  = async (e:any) =>{
 
     const formData = new FormData();
-    //@ts-ignore 
+    //@ts-ignore
     formData.append("file",csv)
     //@ts-ignore
     formData.append("message",message);
@@ -137,45 +151,84 @@ function App() {
  }
   return (
 
-    <div className="App">
+    <div className="App" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+    {/* <h1>Qr-code</h1> */}
+    {/* {!hasConnected&&!qrCode&&<button className="btn btn-primary" onClick={handleGenerateQrCode}  style={{ padding: '10px 20px', fontSize: '1.2rem', cursor: 'pointer' }}>QR-Code Generate</button>} */}
 
-      {qrCode && !hasConnected && <img src={`data:image/png;base64,${String(qrCodeFile)}`}/>}
+    <h1>WhatsApp Web Connect</h1>
+      {qrCode && !hasConnected && <img src={`data:image/png;base64,${String(qrCodeFile)}`} alt="QR Code"
+        style={{ maxWidth: '100%', height: 'auto' }}/>}
 
-     {!hasConnected&&!qrCode&&<button onClick={handleConnection}>Conectar</button>}
+     {!hasConnected&&!qrCode&&<button className="btn btn-primary" onClick={handleConnection}  style={{ padding: '10px 20px', fontSize: '1.2rem', cursor: 'pointer' }}>Conectar</button>}
 
      {hasConnected&&!hasChosenMode&&<>
-     <button onClick={()=>handleModeSelection("csv")}>Enviar por Csv</button>
-     <button onClick={()=>handleModeSelection("specific")}>Enviar para contatos especificos</button>
+     <button className="btn btn-primary" onClick={()=>handleModeSelection("csv")} style={{ padding: '10px 20px', fontSize: '1.2rem', cursor: 'pointer' }}>Enviar por Csv</button>
+     <button className="btn btn-primary" onClick={()=>handleModeSelection("specific")} style={{ padding: '10px 20px', fontSize: '1.2rem', cursor: 'pointer' }}>Enviar para contatos especificos</button>
      </>}
 
     { hasConnected && sendToSpecificContacts&&<>
      {inputList.map((contact, i) => {
         return (
           <>
-          <div className="box">,
-          <h1>Contato {i}</h1>
+          <div className="box" style={{ marginBottom: '1rem' }}>,
+          <h1 style={{ fontSize: '1.5rem' }}>Contato {i}</h1>
             <input
               name="name"
              placeholder="Insíra o nome do contato"
               value={contact.name}
               onChange={e => handleInputChange(e, i)}
+              style={{ padding: '0.5rem', fontSize: '1.2rem' }}
             />
             <input
               className="ml10"
               name="number"
-            placeholder="Insira o número"
+              placeholder="Insira o número"
               value={contact.number}
               onChange={e => handleInputChange(e, i)}
+              style={{ padding: '0.5rem', fontSize: '1.2rem' }}
             />
-            <div className="btn-box">
+            <div className="btn-box" style={{ display: 'flex', justifyContent: 'center', marginTop: '0.5rem' }}>
               {inputList.length !== 1 && <button
+                style={{
+                  padding: '0.5rem 1rem',
+                  fontSize: '1.2rem',
+                  backgroundColor: 'red',
+                  color: '#fff',
+                  border: 'none',
+                  borderRadius: '5px',
+                  marginRight: '0.5rem',
+                  cursor: 'pointer',
+                }}
                 className="mr10"
                 onClick={() => handleRemoveClick(i)}>Remove</button>}
-              {inputList.length - 1 === i && <button onClick={handleAddClick}>Adicionar Contado</button>}
+              {inputList.length - 1 === i && <button
+                style={{
+                  padding: '0.5rem 1rem',
+                  fontSize: '1.2rem',
+                  backgroundColor: '#333',
+                  color: '#fff',
+                  border: 'none',
+                  borderRadius: '5px',
+                  cursor: 'pointer',
+                }}
+                onClick={handleAddClick}>Adicionar Contado</button>}
             </div>
           </div>
-          <input placeholder="Digite a mensagem desejada" value={message} onChange={(e)=>setMessage(e.target.value)}></input>
-          <button onClick={handleSendToSpecifContacts}>Enviar Mensagem</button>
+          <input placeholder="Digite a mensagem desejada"
+            style={{ padding: '0.5rem', fontSize: '1.2rem', marginTop: '1rem' }}
+            value={message} onChange={(e)=>setMessage(e.target.value)}></input>
+          <button
+            className="btn btn-primary"
+            style={{
+              padding: '0.5rem 1rem',
+              fontSize: '1.2rem',
+              backgroundColor: '#333',
+              color: '#fff',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+            onClick={handleSendToSpecifContacts}>Enviar Mensagem</button>
           </>
         );
       })}
@@ -183,12 +236,37 @@ function App() {
   {hasConnected
   &&sendToCSVContactList&&
   <>
+  {/* @ts-ignore*/}
     <input placeholder="Digite a mensagem desejada" value={message} onChange={(e)=>setMessage(e.target.value)}></input>
     {/* @ts-ignore*/}
-    <input type="file" style={{display:'block',marginTop:15}}  onChange={(e)=>setCSV(e.target.files[0])}/>
-    <button onClick={handleSendCSV}>Enviar Mensagem</button>
+    <input type="file" style={{
+        display:'block',
+        marginTop:15,
+        padding: '0.5rem 1rem',
+        fontSize: '1.2rem',
+        backgroundColor: '#333',
+        color: '#fff',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer' }}
+        /* @ts-ignore*/
+        onChange={(e)=>setCSV(e?.target?.files?.[0])}/>
+    <button className="btn btn-primary" onClick={handleSendCSV}>Enviar Mensagem</button>
   </>}
-  {hasConnected&&<button onClick={handleDisconnection} style={{display:'block',marginTop:15}}>Desconectar</button>}
+  {hasConnected&&<button
+    className="btn btn-primary"
+    onClick={handleDisconnection}
+    style={{
+      display:'block',
+      marginTop:15,
+      padding: '0.5rem 1rem',
+      fontSize: '1.2rem',
+      backgroundColor: '#333',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer'
+    }}>Desconectar</button>}
     </div>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
 const api = axios.create({
-    // baseURL:'http://localhost:4000',
-    baseURL:'https://sms-development-server.herokuapp.com/',
+    baseURL:'http://10.10.0.109:4000',
+    // baseURL:'http://10.10.0.109:5003',
+    // baseURL:'https://sms-production-server.herokuapp.com/',
     headers:{
         'Access-Control-Allow-Origin': '*'
     }
 })
-api.defaults.headers.common.Authorization = "Bearer "+process.env.REACT_APP_WHATS_TOKEN
+api.defaults.headers.common.Authorization = "Bearer "+ process.env.REACT_APP_WHATS_TOKEN
 export default api
